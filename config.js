227becca63a0ab45ff5e6c3869fd69fab2b276b1
Bảo Hồ <hophamgiabao4b@gmail.com>
2023-09-02 23:40:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,4 +13,4 @@ const config_phone2 = '17645485273'//手机号码2
 const config_kefu_url = "https://isdkfe.csgyc6isnb.com/chatwindow.aspx?siteId=65000567&planId=af69d421-bf1b-4b89-80f9-4ea0ca7bb53a&_=1684214771078"
 //站点跳转地址
 const config_site_url = "https://www.99win55.com"
-const api = "https://137722.cc"//服务器API地址
+const api = "https://110cwin.com"//服务器API地址
